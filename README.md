efcbec234268f67d4f67baf7f198b048cea9ec94
Daniel Andrade <daniel@danielandrade.net>
2019-03-03 12:51:08 +0000

    Update code and export json.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Compare two csv files
+
+usages `go run fix.go fileOld.csv fileNew.csv`.
--- fix.go
+++ fix.go
@@ -3,8 +3,10 @@ package main
 import (
 	"bufio"
 	"encoding/csv"
+	"encoding/json"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"os"
 )
@@ -32,18 +34,43 @@ func Contains(a []string, x string) bool {
 	return false
 }
 
+// Reads the CSV File and return a list of companies
+func getNames(fileName string) []string {
+	var oldList []string
+	csvFileOld, _ := os.Open(fileName)
+	readerOld := csv.NewReader(bufio.NewReader(csvFileOld))
+	for {
+		line, error := readerOld.Read()
+		if error == io.EOF {
+			break
+		} else if error != nil {
+			log.Fatal(error)
+		}
+
+		oldList = append(oldList, line[0])
+	}
+	return oldList
+}
+
 func main() {
 
 	oldFile := os.Args[1]
 	newFile := os.Args[2]
 
+	oldCompanyList := getNames(oldFile)
+	newCompanyList := getNames(newFile)
+
+	fmt.Printf("Old company #%d.\n", len(oldCompanyList))
+	fmt.Printf("New company #%d.\n", len(newCompanyList))
+
+	// finalList := generateFinalList(newCompanyList, oldCompanyList)
+
+	// Generate complete list
+	var completeList []string
 	csvFile, _ := os.Open(oldFile)
 	reader := csv.NewReader(bufio.NewReader(csvFile))
-
 	var company []Company
-	var nameList []string // Temporary list with only names
 
-	i := 0
 	for {
 		line, error := reader.Read()
 
@@ -53,81 +80,34 @@ func main() {
 			log.Fatal(error)
 		}
 
-		nameList = append(nameList, line[0])
-
-		company = append(company, Company{
-			Name:      line[0],
-			Website:   line[1],
-			Twitter:   line[2],
-			Instagram: line[3],
-			Facebook:  line[4],
-			Linkedin:  line[5],
-			Youtube:   line[6],
-			Github:    line[7],
-		})
-
-		i++
-	}
-
-	// Parse second file, and add just in case it is not alrady there
-	csvFile, _ = os.Open(newFile)
-	reader = csv.NewReader(bufio.NewReader(csvFile))
-	j := 0
-
-	var newList []string
-
-	added := 0
-	for {
-		line, error := reader.Read()
-
-		if error == io.EOF {
-			break
-		} else if error != nil {
-			log.Fatal(error)
-		}
-
-		// Make a list of only new items
-		newList = append(newList, line[0])
-
-		// If company doesn't exist in old list, add it.
-		if Contains(nameList, line[0]) == false {
+		// Only append if old name is in the new list also
+		if Contains(newCompanyList, line[0]) {
+			completeList = append(completeList, line[0])
 			company = append(company, Company{
-				Name: line[0],
+				Name:      line[0],
+				Website:   line[1],
+				Twitter:   line[2],
+				Instagram: line[3],
+				Facebook:  line[4],
+				Linkedin:  line[5],
+				Youtube:   line[6],
+				Github:    line[7],
 			})
-			nameList = append(nameList, line[0])
-			fmt.Printf("[%s] was added to the list!\n", line[0])
-			added++
 		}
 
-		j++
 	}
-
-	// Check if old is present in new, otherwise remove it
-	csvFile, _ = os.Open(oldFile)
-	reader = csv.NewReader(bufio.NewReader(csvFile))
-	removed := 0
-	for {
-		line, error := reader.Read()
-
-		if error == io.EOF {
-			break
-		} else if error != nil {
-			log.Fatal(error)
-		}
-
-		// Check if old is in new list
-		if !Contains(newList, line[0]) {
-			fmt.Printf("[%s] was removed from the list!\n", line[0])
-			removed++
+	// Add remaining new companies in Company list
+	for _, c := range newCompanyList {
+		if !Contains(completeList, c) {
+			// Add remaining new list to company
+			company = append(company, Company{
+				Name: c,
+			})
 		}
 	}
 
-	fmt.Printf("Total %d - %d added and %d removed.\n", len(nameList), added, removed)
+	companyJSON, _ := json.Marshal(company)
+	ioutil.WriteFile("output.json", companyJSON, 0644)
+	fmt.Println(string(companyJSON))
 
-	// companyJSON, _ := json.Marshal(company)
-	// ioutil.WriteFile("output.json", companyJSON, 0644)
-	// fmt.Println(string(companyJSON))
-
-	// diff := j - i
-	// fmt.Printf("There are %d companies in old and %d in new. Diff %d\n", i, j, diff)
 }
